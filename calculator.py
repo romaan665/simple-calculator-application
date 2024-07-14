c60ef538c823ece22f65fcb1f83ef7bcfdbe89c2
romaan <153754885+romaan665@users.noreply.github.com>
2024-07-14 15:30:23 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+
+import sys
+from PyQt6.QtWidgets import QApplication,QLabel,QWidget 
+
+app=QApplication([])
+
+window= QWidget()
+window.setWindowTitle("PyQt App")
+window.setGeometry(100,100,280,80)
+helloMsg=QLabel("<h1>hello,world!</h1>",parent=window)
+helloMsg.move(60,15)
+
+window.show()
+
+
+
